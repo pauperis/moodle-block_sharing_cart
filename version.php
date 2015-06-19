5aacaf044d67fb2782df63060b5f575fb03e7dc6
yishe <y-choden@ver2.jp>
2015-06-19 15:33:36 +0900

    Bug fix

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015012700;
+$plugin->version   = 2015061600;
 $plugin->requires  = 2012062501.00; // Moodle 2.3.1
 $plugin->component = 'block_sharing_cart';
-$plugin->release   = '2.6, release 1 patch 7';
+$plugin->release   = '2.9, release 1';
 $plugin->maturity  = MATURITY_STABLE;
